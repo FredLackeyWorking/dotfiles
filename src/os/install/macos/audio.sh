d147bc98cfa02bbb7db1c2e03d5f9bba69adb7ef
Fred Lackey <fred.lackey@gmail.com>
2017-09-25 19:07:30 -0400

    remove amazon music from macOS

--- src/os/install/macos/audio.sh
+++ src/os/install/macos/audio.sh
@@ -10,7 +10,7 @@ print_in_purple "\n   Audio\n\n"
 
 brew_install "Airfoil" "airfoil" "caskroom/cask" "cask"
 # brew_install "Android File Transfer" "android-file-transfer" "caskroom/cask" "cask"
-brew_install "Amazon Music" "amazon-music" "caskroom/cask" "cask"
+# brew_install "Amazon Music" "amazon-music" "caskroom/cask" "cask"
 brew_install "Audio Hijack" "audio-hijack" "caskroom/cask" "cask"
 # brew_install "Background Music" "background-music" "caskroom/cask" "cask"
 # brew_install "djay Pro" "djay-pro" "caskroom/cask" "cask"
